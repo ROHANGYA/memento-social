847d1663f79002c1a6cb59aff2f1efdc082c0ab9
Rohan <rohgyas@hotmail.com>
2025-07-09 11:17:47 +0800

    Add query client provider

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { I18nextProvider } from "react-i18next";
 import { i18nLocale } from "./src/lang/lang";
 import { getLoadedFonts, useFonts } from "expo-font";
 import { FontAssets } from "./src/utils/assetUtil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
   const [loaded, error] = useFonts({
@@ -15,13 +16,17 @@ export default function App() {
   });
   console.log(`Loaded fonts: ${getLoadedFonts()}`);
 
+  const queryClient = new QueryClient();
+
   return (
-    <Provider store={globalStore}>
-      <I18nextProvider i18n={i18nLocale}>
-        <PaperProvider theme={appTheme}>
-          <AppNavigation />
-        </PaperProvider>
-      </I18nextProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={globalStore}>
+        <I18nextProvider i18n={i18nLocale}>
+          <PaperProvider theme={appTheme}>
+            <AppNavigation />
+          </PaperProvider>
+        </I18nextProvider>
+      </Provider>
+    </QueryClientProvider>
   );
 }
